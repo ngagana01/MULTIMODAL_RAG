e802031240ff21f0f2c2a0b913549a541f5bb14e
Gagana N <ngagana01@gmail.com>
2026-02-13 10:26:06 +0530

    Update retriever.py

--- rag_/retriever.py
+++ rag_/retriever.py
@@ -1,25 +1,15 @@
-import faiss
 import numpy as np
-import os
-from config import INDEX_PATH
 
 class Retriever:
     def __init__(self, dim):
-        self.index = faiss.IndexFlatL2(dim)
-        self.texts = []
+        self.vectors=[]
+        self.texts=[]
 
     def add(self, vectors, texts):
-        arr = np.array(vectors).astype("float32")
-        self.index.add(arr)
+        self.vectors.extend(vectors)
         self.texts.extend(texts)
 
     def search(self, vector, k=5):
-        D,I = self.index.search(np.array([vector]).astype("float32"), k)
-        return [self.texts[i] for i in I[0]]
-
-    def save(self):
-        faiss.write_index(self.index, INDEX_PATH)
-
-    def load(self):
-        if os.path.exists(INDEX_PATH):
-            self.index = faiss.read_index(INDEX_PATH)
+        sims=[np.dot(vector,v) for v in self.vectors]
+        idx=np.argsort(sims)[::-1][:k]
+        return [self.texts[i] for i in idx]
